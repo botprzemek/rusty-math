0247a27299dbe14d4752f8e5200a254b2a7740da
botprzemek <info@botprzemek.pl>
2024-06-25 08:14:33 +0200

    feat(app): unfinished rnp

--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,97 @@
-fn derivative(n: i32) -> i32 {
-    // f(x) = 3 * x ^ 2
-    // f'(x) = 3 * 2 * x ^ 1
-
-    let mut x = 2;
-
-    for number in 1..n - 1 {
-        if number == n {
-            break;
-        }
-
-        x *= x;
-
-        println!("X: {}", x);
-    }
-
-    return x * n;
+struct Stack<T> {
+    stack: Vec<T>,
 }
 
-// struct Stack<T> {
-//     stack: Vec<T>,
-// }
+impl<T> Stack<T> {
+    fn new() -> Self {
+      Stack {
+          stack: Vec::new(),
+      }
+    }
 
-// impl<T> Stack<T> {
-//     fn new() -> Self {
-//       Stack { stack: Vec::new() }
-//     }
+    fn get(&self) -> &Vec<T> {
+        &self.stack
+    }
 
-//     fn empty(&mut self) {
-//         self.stack.clear();
-//     }
+    fn pop(&mut self) -> Option<T> {
+        self.stack.pop()
+    }
 
-//     fn pop(&mut self) -> Option<T> {
-//         self.stack.pop()
-//     }
+    fn push(&mut self, item: T) {
+        self.stack.push(item);
+    }
+}
 
-//     fn push(&mut self, item: T) {
-//         self.stack.push(item);
-//     }
+fn transform_expression(expression: &str) -> Stack<String> {
+    let input: Vec<&str> = expression.split(" ").collect();
+    let mut output: Stack<String> = Stack::new();
+    let mut operators: Stack<char> = Stack::new();
 
-//     fn top(&mut self) -> Option<&T> {
-//         self.stack.first()
-//     }
-// }
+    for text in input {
+        match text.parse::<f32>() {
+            Ok(number) => {
+                println!("Push \"{}\" to output", number);
+                output.push(number.to_string());
+            }
+            Err(_) => {
+                if let Some(operator) = text.chars().collect::<Vec<char>>().get(0) {
+                    match operator {
+                        '+' | '-' | '^' | '(' => {
+                            println!("Push \"{}\" to stack", operator);
+                            operators.push(*operator);
+                        },
+                        '/' | '*' | '%' => {
+                            if let Some(operator) = operators.pop() {
+                                if operator != '(' {
+                                    println!("Push \"{}\" to output", operator);
+                                    output.push(operator.to_string());
+                                }
+                            }
 
-// fn transform_expression(expression: &str) -> Stack<String> {
-//     // (2+3)*5  =>  ["2", "3", "+", "5", "*"]
+                            println!("Push \"{}\" to output", operator);
+                            output.push(operator.to_string());
+                        },
+                        ')' => {
+                            for _ in 0..operators.get().len() {
+                                if let Some(operator) = operators.pop() {
+                                    if operator == '(' {
+                                        break;
+                                    }
 
-//     let input: Vec<&str> = expression.split(' ').collect();
-//     let mut output: Stack<String> = Stack::new();
-//     let mut operators: Stack<char> = Stack::new();
+                                    println!("Push \"{}\" to output", operator);
+                                    output.push(operator.to_string());
+                                }
+                            }
+                        },
+                        _ => {},
+                    }
+                }
+            }
+        }
+    }
 
-//     for value in input {
-//         let test: Vec<char> = value.chars().collect();
+    for _ in 0..operators.get().len() {
+        if let Some(operator) = operators.pop() {
+            if operator != '(' {
+                println!("Old. Push \"{}\" to output", operator);
+                output.push(operator.to_string());
+            }
+        }
+    }
 
-//         if test.len() == 1 {
-//             match test.get(0) {
-//                 None => (),
-//                 Some(operator) => {
-//                     match operator {
-//                         '+' | '-' | '*' | '/' | '^' | '%' | '(' | ')' => {
-//                             operators.push(*operator);
-//                         },
-//                         _ => (),
-//                     }
-//                 },
-//             }
-//         }
+    return output;
+}
 
-//         else {
-//             let number = value.trim().parse::<i64>();
+const TEST_EXPRESSION: &str = "3 + 4 * 2 / ( 1 − 5 ) ^ 2";
+const TEST_RESULT: &str = "3 4 2 * 1 5 − 2 ^ / +";
 
-//             match number {
-//                 Ok(ok) => {
-//                     output.push(ok);
-//                     print!("{:?}", output)
-//                 },
-//                 Err(error) => print!("{}", error),
-//             }
-//         }
-//     }
-
-//     return output;
-// }
-
-// fn calculate_postfix(expression: &str) -> i32 {
-//     let mut result = 0;
-
-//     for value in transform_expression(expression).filter(|value| *value != ' ') {
-//         match value {
-//             '0'..='9' => operation.push(value),
-//             '+' | '-' | '*' | '/' | '^' | '%' => print!("operator, "),
-//             _ => (),
-//         }
-
-//         result += 1;
-//     }
-
-//     return result;
-// }
-
-// const TEST_EXPRESSION: &str = "(2+3)*5";
-
-// fn test_postfix() {
-//     let result = calculate_postfix(TEST_EXPRESSION);
-//     print!("Test Result: {}", result);
-//     assert!(result == 25);
-// }
+fn test_postfix() {
+    assert_eq!(
+        TEST_RESULT,
+        transform_expression(TEST_EXPRESSION).get().join(" "),
+    );
+}
 
 fn main() {
-    println!("Pochodna z x^3: {}", pochodna(3));
+    test_postfix();
 }
