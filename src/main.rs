6dcb8c852b7299817f3de18e8e18bbc99dcf9aea
Przemysław Szymański <przemyslaw.szymanski@flytronic.pl>
2024-06-25 08:35:59 +0200

    feat(app): temporary removed operator transform for debugging

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ struct Stack<T> {
 
 impl<T> Stack<T> {
     fn new() -> Self {
-      Stack {
-          stack: Vec::new(),
-      }
+        Stack {
+            stack: Vec::new(),
+        }
     }
 
     fn get(&self) -> &Vec<T> {
@@ -30,41 +30,11 @@ fn transform_expression(expression: &str) -> Stack<String> {
     for text in input {
         match text.parse::<f32>() {
             Ok(number) => {
-                println!("Push \"{}\" to output", number);
                 output.push(number.to_string());
             }
             Err(_) => {
                 if let Some(operator) = text.chars().collect::<Vec<char>>().get(0) {
-                    match operator {
-                        '+' | '-' | '^' | '(' => {
-                            println!("Push \"{}\" to stack", operator);
-                            operators.push(*operator);
-                        },
-                        '/' | '*' | '%' => {
-                            if let Some(operator) = operators.pop() {
-                                if operator != '(' {
-                                    println!("Push \"{}\" to output", operator);
-                                    output.push(operator.to_string());
-                                }
-                            }
-
-                            println!("Push \"{}\" to output", operator);
-                            output.push(operator.to_string());
-                        },
-                        ')' => {
-                            for _ in 0..operators.get().len() {
-                                if let Some(operator) = operators.pop() {
-                                    if operator == '(' {
-                                        break;
-                                    }
-
-                                    println!("Push \"{}\" to output", operator);
-                                    output.push(operator.to_string());
-                                }
-                            }
-                        },
-                        _ => {},
-                    }
+                    
                 }
             }
         }
@@ -72,18 +42,15 @@ fn transform_expression(expression: &str) -> Stack<String> {
 
     for _ in 0..operators.get().len() {
         if let Some(operator) = operators.pop() {
-            if operator != '(' {
-                println!("Old. Push \"{}\" to output", operator);
-                output.push(operator.to_string());
-            }
+            output.push(operator.to_string());
         }
     }
 
     return output;
 }
 
-const TEST_EXPRESSION: &str = "3 + 4 * 2 / ( 1 − 5 ) ^ 2";
-const TEST_RESULT: &str = "3 4 2 * 1 5 − 2 ^ / +";
+const TEST_EXPRESSION: &str = "3 + 4 * 2 / ( 1 - 5 ) ^ 2";
+const TEST_RESULT: &str = "3 4 2 * 1 5 - 2 ^ / +";
 
 fn test_postfix() {
     assert_eq!(
