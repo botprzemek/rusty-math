6141921a0047e91e028f99b5d8b8cf581d25dbd2
botprzemek <info@botprzemek.pl>
2024-06-28 11:27:44 +0200

    feat(app): extra formatting for more readability of postfix module

--- src/notations/mod.rs
+++ src/notations/mod.rs
@@ -1,61 +1,54 @@
 use crate::structs::Stack;
 
-fn get_precedence(operator: char) -> u8 {
+fn get_precedence(operator: &str) -> u8 {
     match operator {
-        '+' | '-' => 1,
-        '*' | '/' | '%' => 2,
-        '^' => 3,
+        "+" | "-" => 1,
+        "*" | "/" | "%" => 2,
+        "^" => 3,
         _ => 0,
     }
 }
 
-fn handle_operator(operator: char, output: &mut Stack<String>, operators: &mut Stack<char>) {
+fn handle_operator<'a>(operator: &'a str, output: &mut Stack<&'a str>, operators: &mut Stack<&'a str>) {
     match operator {
-        '+' | '-' | '*' | '/' | '%' | '^' => {
+        "+" | "-" | "*" | "/" | "%" | "^" => {
             while let Some(top) = operators.top() {
                 if get_precedence(*top) < get_precedence(operator) {
                     break;
                 }
 
-                output.push((*top).to_string());
+                output.push(*top);
                 operators.pop();
             }
 
             operators.push(operator);
         }
-        ')' => {
+        ")" => {
             while let Some(top) = operators.top() {
-                if *top == '(' {
+                if *top == "(" {
                     operators.pop();
                     break;
                 }
 
-                output.push((*top).to_string());
+                output.push(*top);
                 operators.pop();
             }
         }
-        '(' => operators.push(operator),
-        _ => output.push(operator.to_string()),
+        "(" => operators.push(operator),
+        _ => output.push(operator),
     }
 }
 
-pub fn postfix(expression: &str) -> Stack<String> {
-    let mut output: Stack<String> = Stack::new();
-    let mut operators: Stack<char> = Stack::new();
+pub fn postfix(expression: &str) -> Stack<&str> {
+    let mut output: Stack<&str> = Stack::new();
+    let mut operators: Stack<&str> = Stack::new();
 
-    for token in expression.split_whitespace() {
-        match token.parse::<f32>() {
-            Ok(number) => output.push(number.to_string()),
-            Err(_) => {
-                if let Some(operator) = token.chars().next() {
-                    handle_operator(operator, &mut output, &mut operators)
-                }
-            },
-        }
-    }
+    expression
+        .split_whitespace()
+        .for_each(| operator| handle_operator(operator, &mut output, &mut operators));
 
     while let Some(operator) = operators.pop() {
-        output.push(operator.to_string());
+        output.push(operator);
     }
 
     output
